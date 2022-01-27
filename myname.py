64ae8c8fab7921f0ed77fba03d26f4870757c0ac
Kossi Bissadu <kbissadu@zennerusa.com>
2022-01-27 17:56:01 -0600

    My First Assignment adding myname.py to the repository

---
+++ myname.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#Print my full name
+print('My Name is Kossi Dodzi  Bissadu')
+
+# Request my full name through a user input
+fName = input("\nPlease enter your first name:")
+mName = input("\nPlease enter your middle name: ")
+lName = input("\nPlease enter your last name:")
+
+print('\nMy name is :', fName, mName, lName)
