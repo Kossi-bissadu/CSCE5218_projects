bd6357d736979ba827c704276c144bfa354f3751
Kossi Bissadu <kbissadu@zennerusa.com>
2022-01-27 18:05:14 -0600

    Assignment 1 for name

--- myname.py
+++ myname.py
@@ -7,5 +7,5 @@ print('My Name is Kossi Dodzi  Bissadu')
 fName = input("\nPlease enter your first name:")
 mName = input("\nPlease enter your middle name: ")
 lName = input("\nPlease enter your last name:")
-
+#print the fulll name
 print('\nMy name is :', fName, mName, lName)
